decf950cf13471bd2ce8b60cbb1b1c1f7b47a631
Justin Bitely <jbitely@gmail.com>
2016-01-19 20:01:47 -0500

    Refactored .step in showyDancer to invoke .showOff when within a given distance of another dancer.

--- src/showyDancer.js
+++ src/showyDancer.js
@@ -4,21 +4,23 @@ var ShowyDancer = function (top, left, timeBetweenSteps) {
   this.$node.addClass('showy-dancer');
   this.dancersIndex = window.dancers.length;
   console.log(window.dancers.length);
-}
+};
 
 ShowyDancer.prototype = Object.create(BadDancer.prototype);
 ShowyDancer.prototype.constructor = ShowyDancer;
 
 ShowyDancer.prototype.step = function () {
   BadDancer.prototype.step.call(this);
-  var showingOff = false;
   for (var i = 0; i < window.dancers.length; i++) {
     if (this.dancersIndex !== i) {
-      if (window.dancers[i].top - this.top)
+      if (this.distance(window.dancers[i].top, window.dancers[i].left) < 100) {
+        this.showOff();
+        break;
+      }
     }
   }
-}
+};
 
 ShowyDancer.prototype.showOff = function () {
   alert("I'm showing off!");
-}
+};
